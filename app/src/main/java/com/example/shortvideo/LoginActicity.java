c37f3f6ffd67d973d2b6ee9c6b61b62d28ec94f0
phamhoanghuy2004 <phamhoanghuy.2000vn@gmail.com>
2025-05-09 07:55:19 +0700

    Update LoginActicity.java

--- app/src/main/java/com/example/shortvideo/LoginActicity.java
+++ app/src/main/java/com/example/shortvideo/LoginActicity.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class LoginActicity extends AppCompatActivity {
     private EditText etEmail, etPassword;
-    private Button btnLogin,btnSignUp2;
+    private Button btnLogin,btnSignUp3;
 
 
     @Override
